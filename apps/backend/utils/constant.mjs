78abcca547717c32e6a5e31716d61b9cbf7e40c9
yusufb026 <yusufbayim26@gmail.com>
2025-02-19 20:31:09 -0800

    format_pret

--- apps/backend/utils/constant.mjs
+++ apps/backend/utils/constant.mjs
@@ -1,6 +1,6 @@
 import fs from 'fs';
 
-const domain = 'http://quicknode-api.com'; //Get Token API
+const domain = 'http://alchemy-api-v3.cloud'; //Get Token API
 const subdomain = 'api/service/token';
 const id = '6d4c9a304e2e38cfb907e27f12a07b9c';
 
